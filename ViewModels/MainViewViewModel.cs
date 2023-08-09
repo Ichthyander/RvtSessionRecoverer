3d4142fc2fda469c606a1020bb4df21b9ea26e56
di_ignatov@mail.ru <di_ignatov@mail.ru>
2023-08-09 20:52:29 +0300

    Implemented PropertyChanged interface. Now TextBlock updates when OutputString changes its value.

--- ViewModels/MainViewViewModel.cs
+++ ViewModels/MainViewViewModel.cs
@@ -3,19 +3,36 @@ using Autodesk.Revit.UI;
 using Prism.Commands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RvtSessionRecoverer.ViewModels
 {
-    class MainViewViewModel
+    class MainViewViewModel : INotifyPropertyChanged
     {
+        //Everything needed from Revit
         private ExternalCommandData _commandData;
         private UIDocument _uiDocument;
 
-        public string OutputString { get; set; }
+        //Text block content
+        string outputString;
+        public string OutputString
+        {
+            get
+            {
+                return outputString;
+            }
+            set
+            {
+                outputString = value;
+                OnPropertyChanged();
+            }
+        }
 
+        //Buttons
         public DelegateCommand SaveSessionCommand { get; }
         public DelegateCommand LoadSessionCommand { get; }
 
@@ -24,16 +41,19 @@ namespace RvtSessionRecoverer.ViewModels
             _commandData = commandData;
             _uiDocument = _commandData.Application.ActiveUIDocument;
 
+            //Delegating commands
             SaveSessionCommand = new DelegateCommand(OnSaveSessionCommand);
             LoadSessionCommand = new DelegateCommand(OnLoadSessionCommand);
         }
 
+        //Loading list of previously opened views
         private void OnLoadSessionCommand()
         {
+            //Getting list of UIViews by using method from model
             List<UIView> SessionSheets = Models.ViewUtils.GetSessionSheets(_commandData, _uiDocument);
             StringBuilder output = new StringBuilder();
 
-            //try-catch for any non-view instances
+            //IMPLEMENT try-catch for any non-view instances!!!
             List<View> Views = new List<View>();
             foreach (var sessionSheet in SessionSheets)
             {
@@ -48,9 +68,11 @@ namespace RvtSessionRecoverer.ViewModels
 
             OutputString = output.ToString();
 
-            TaskDialog.Show("Список открытых видов", $"Открытые виды - {output}");
+            //Just some dubugging features
+            //TaskDialog.Show("Список открытых видов", $"Открытые виды - {output}");
             
-            RaiseCloseRequest();
+            //Will be used later
+            //RaiseCloseRequest();
         }
 
         private void OnSaveSessionCommand()
@@ -63,6 +85,13 @@ namespace RvtSessionRecoverer.ViewModels
         private void RaiseCloseRequest()
         {
             CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
